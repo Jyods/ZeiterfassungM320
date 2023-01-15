5b5bbc625d90759e227637da20d9c2da1e6f7060
Maxim Grob <maxim.grob@schindler.com>
2023-01-15 20:48:12 +0100

    added test project

---
+++ ZeiterfassungM320Tests/TestLernender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeiterfassungsprogrammTests {
+
+	[TestFixture]
+	class TestLernender {
+
+		[Test]
+		public void TestSetFerien() {
+
+		}
+
+		[Test]
+		public void TestSetLohnzuschlag() {
+
+		}
+
+	}
+}
